0dd4903c11cc58b0271b035b8e67e5b6355ae411
kohei.sato <night.god.moon.garsu@gmail.com>
2021-06-02 14:12:06 +0900

    SGを設定して外部からアクセスできるように

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 
 
 # 実行時に上書き可能
+# 例：
 # terraform plan -var "sugar_instance_type=t3.nano"
 variable "sugar_instance_type" {
 	default = "t2.micro"
@@ -30,9 +31,28 @@ data "aws_ami" "recent_amazon_linux_2" {
 	}
 }
 
-resource "aws_instance" "example" {
+resource "aws_security_group" "sugar_tf_sg" {
+	name = "sugar-ec2"
+
+	ingress {
+	  to_port = 80
+	  from_port = 80
+	  protocol = "tcp"
+	  cidr_blocks = [ "0.0.0.0/0" ]
+	}
+
+	egress {
+	  to_port = 0
+	  from_port = 0
+	  protocol = "-1"
+	  cidr_blocks = [ "0.0.0.0/0" ]
+	}
+}
+
+resource "aws_instance" "sugar_tf_ec2" {
 	ami		= data.aws_ami.recent_amazon_linux_2.image_id
 	instance_type   = var.sugar_instance_type
+	vpc_security_group_ids = [aws_security_group.sugar_tf_sg.id]
 	
 	tags = {
 		Name = local.sugar_tag
@@ -47,14 +67,18 @@ EOF
 
 # outputは実行結果後に特定の値を出力可能
 output "instance_id" {
-	value = aws_instance.example.id
+	value = aws_instance.sugar_tf_ec2.id
 }
 output "ami" {
-	value = aws_instance.example.ami
+	value = aws_instance.sugar_tf_ec2.ami
 }
 output "private_ip" {
-	value = aws_instance.example.private_ip
+	value = aws_instance.sugar_tf_ec2.private_ip
 }
 output "public_ip" {
-	value = aws_instance.example.public_ip
+	value = aws_instance.sugar_tf_ec2.public_ip
+}
+
+output "public_dns" {
+	value = aws_instance.sugar_tf_ec2.public_dns
 }
