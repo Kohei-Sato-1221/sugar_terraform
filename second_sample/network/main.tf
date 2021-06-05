05d24211551ff5406c6a45198652d82c7e17c2be
kohei.sato <night.god.moon.garsu@gmail.com>
2021-06-05 14:24:11 +0900

    add public subnet

---
+++ second_sample/network/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+    region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "sugar_vpc" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+
+    tags = {
+        Name = "sugar_vpc"
+    }
+}
+
+resource "aws_subnet" "sugar_public" {
+    vpc_id = aws_vpc.sugar_vpc.id
+    cidr_block = "10.0.0.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_internet_gateway" "sugar_ig" {
+    vpc_id = aws_vpc.sugar_vpc.id
+}
+
+resource "aws_route_table" "sugar_public" {
+    vpc_id = aws_vpc.sugar_vpc.id
+}
+
+resource "aws_route" "sugar_public" {
+    route_table_id = aws_route_table.sugar_public.id
+    gateway_id = aws_internet_gateway.sugar_ig.id
+    destination_cidr_block = "0.0.0.0/0"
+}
+
+#resource "aws_route_table_association" "sugar_private" {
+#    vpc_id = aws_vpc.sugar_vpc.id
+#    cidr_block = "10.0.64.0/24"
+#    availability_zone = "ap-northeast-1a"
+#    map_public_ip_on_launch = false
+#}
