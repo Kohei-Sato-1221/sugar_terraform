2acf300401ba00a8e8e9b01ffed2efbf46fd490c
kohei.sato <night.god.moon.garsu@gmail.com>
2021-06-05 15:11:27 +0900

    multi-az

--- second_sample/network/main.tf
+++ second_sample/network/main.tf
@@ -12,11 +12,18 @@ resource "aws_vpc" "sugar_vpc" {
     }
 }
 
-resource "aws_subnet" "sugar_public" {
+resource "aws_subnet" "sugar_public_0" {
     vpc_id = aws_vpc.sugar_vpc.id
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "10.0.1.0/24"
     map_public_ip_on_launch = true
     availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_subnet" "sugar_public_1" {
+    vpc_id = aws_vpc.sugar_vpc.id
+    cidr_block = "10.0.2.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "ap-northeast-1c"
 }
 
 resource "aws_internet_gateway" "sugar_ig" {
@@ -33,40 +40,101 @@ resource "aws_route" "sugar_public" {
     destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_route_table_association" "sugar_public" {
-    subnet_id = aws_subnet.sugar_public.id
+resource "aws_route_table_association" "sugar_public_0" {
+    subnet_id = aws_subnet.sugar_public_0.id
     route_table_id = aws_route_table.sugar_public.id
 }
 
-resource "aws_subnet" "sugar_private" {
+resource "aws_route_table_association" "sugar_public_1" {
+    subnet_id = aws_subnet.sugar_public_0.id
+    route_table_id = aws_route_table.sugar_public.id
+}
+
+resource "aws_subnet" "sugar_private_0" {
     vpc_id = aws_vpc.sugar_vpc.id
-    cidr_block = "10.0.64.0/24"
+    cidr_block = "10.0.65.0/24"
     availability_zone = "ap-northeast-1a"
     map_public_ip_on_launch = false
 }
 
-resource "aws_route_table" "sugar_private" {
+resource "aws_subnet" "sugar_private_1" {
+    vpc_id = aws_vpc.sugar_vpc.id
+    cidr_block = "10.0.66.0/24"
+    availability_zone = "ap-northeast-1c"
+    map_public_ip_on_launch = false
+}
+
+resource "aws_route_table" "sugar_private_0" {
     vpc_id = aws_vpc.sugar_vpc.id
 }
 
-resource "aws_route_table_association" "sugar_private" {
-    subnet_id = aws_subnet.sugar_private.id
-    route_table_id = aws_route_table.sugar_private.id
+resource "aws_route_table" "sugar_private_1" {
+    vpc_id = aws_vpc.sugar_vpc.id
 }
 
-resource "aws_eip" "sugar_ng_eip" {
+resource "aws_route" "sugar_private_0" {
+    route_table_id = aws_route_table.sugar_private_0.id
+    nat_gateway_id = aws_nat_gateway.sugar_ng_0.id
+    destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "sugar_private_1" {
+    route_table_id = aws_route_table.sugar_private_1.id
+    nat_gateway_id = aws_nat_gateway.sugar_ng_1.id
+    destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "sugar_private_0" {
+    subnet_id = aws_subnet.sugar_private_0.id
+    route_table_id = aws_route_table.sugar_private_0.id
+}
+
+resource "aws_route_table_association" "sugar_private_1" {
+    subnet_id = aws_subnet.sugar_private_1.id
+    route_table_id = aws_route_table.sugar_private_1.id
+}
+
+resource "aws_eip" "sugar_ng_eip_0" {
     vpc = true
     depends_on = [aws_internet_gateway.sugar_ig]
 }
 
-resource "aws_nat_gateway" "sugar_ng" {
-    allocation_id = aws_eip.sugar_ng_eip.id
-    subnet_id = aws_subnet.sugar_public.id
+resource "aws_eip" "sugar_ng_eip_1" {
+    vpc = true
     depends_on = [aws_internet_gateway.sugar_ig]
 }
 
-resource "aws_route" "sugar_private" {
-    route_table_id = aws_route_table.sugar_private.id
-    nat_gateway_id = aws_nat_gateway.sugar_ng.id
-    destination_cidr_block = "0.0.0.0/0"
+resource "aws_nat_gateway" "sugar_ng_0" {
+    allocation_id = aws_eip.sugar_ng_eip_0.id
+    subnet_id = aws_subnet.sugar_public_0.id
+    depends_on = [aws_internet_gateway.sugar_ig]
+}
+
+resource "aws_nat_gateway" "sugar_ng_1" {
+    allocation_id = aws_eip.sugar_ng_eip_1.id
+    subnet_id = aws_subnet.sugar_public_1.id
+    depends_on = [aws_internet_gateway.sugar_ig]
+}
+
+resource "aws_security_group" "sugar_sg" {
+    name = "sugar_sg"
+    vpc_id = aws_vpc.sugar_vpc.id
+}
+
+resource "aws_security_group_rule" "sugar_ingress" {
+    type = "ingress"
+    from_port = "80"
+    to_port = "80"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.sugar_sg.id
+}
+
+resource "aws_security_group_rule" "sugar_egress" {
+    type = "egress"
+    from_port = "0"
+    to_port = "0"
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = aws_security_group.sugar_sg.id
 }
